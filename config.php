d8e316f59422b32e4d2c7ce11c4848658807d6c0
Eduard Stefan <eduardstefan290@gmail.com>
2020-10-05 21:15:56 +0200

    fixed session problem from not working on first visit

--- config.php
+++ config.php
@@ -1,15 +1,15 @@
 <?php
 
 //Is this the production server or not?
-define('DEV_TYPE', "PRODUCTION_TEST");
+define('DEV_TYPE', "LOCAL");
 
 //Set the site URL.
 if (DEV_TYPE == "PRODUCTION") {
   //The URL of our live website.
-  define('SITE_URL', 'http://dws-practicas.herokuapp.com/');
+  define('SITE_URL', 'https://dws-practicas.herokuapp.com/');
 } elseif (DEV_TYPE == "PRODUCTION_TEST") {
   //The URL that we use in our development enviornment.
-  define('SITE_URL', 'http://dws-practicas-dev.herokuapp.com/');
+  define('SITE_URL', 'https://dws-practicas-dev.herokuapp.com/');
 } else {
   define('SITE_URL', 'http://localhost/dws/practicas/');
 }
--- scripts/storeDetailsMenu.js
+++ scripts/storeDetailsMenu.js
@@ -4,9 +4,14 @@ let navbarScrollTop = sessionStorage.getItem("navbarScrollTop");
 
 // Store the details open property in sessions so if it was opened before reloading or navigating to other page it will still be opened or closed
 detailsElems.forEach((element) => {
-  let isOpen = sessionStorage.getItem(element.id) == "true";
-  if (isOpen) {
-    element.open = isOpen;
+  let isOpen = sessionStorage.getItem(element.id);
+
+  // check if session value exists at all
+  if (isOpen == null) {
+    sessionStorage.setItem(element.id, false);
+  } else {
+    // if it exists check if the value is true or false
+    element.open = isOpen == "true";
   }
 
   element.addEventListener("toggle", (event) => {
