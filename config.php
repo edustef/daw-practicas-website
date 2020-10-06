1c10274c0cb63d02944095c5f9e5e52eca130107
Eduard Stefan <eduardstefan290@gmail.com>
2020-10-06 20:25:50 +0200

    ready for testing

--- config.php
+++ config.php
@@ -4,15 +4,20 @@ define("PRODUCTION", "PRODUCTION");
 define("PRODUCTION_TEST", "PRODUCTION_TEST");
 define("LOCAL", "LOCAL");
 
-define('SERVER_TYPE', PRODUCTION);
+define('SERVER_TYPE', PRODUCTION_TEST);
 
-//Set the site URL.
-if (SERVER_TYPE == PRODUCTION) {
-  //The URL of our live website.
-  define('SITE_URL', 'https://dws-practicas.herokuapp.com/');
-} elseif (SERVER_TYPE == PRODUCTION_TEST) {
-  //The URL that we use in our development enviornment.
-  define('SITE_URL', 'https://dws-practicas-dev.herokuapp.com/');
-} elseif (SERVER_TYPE == LOCAL) {
-  define('SITE_URL', 'http://localhost/dws/practicas/');
-}
+echo $_SERVER["SERVER_NAME"];
+echo $_SERVER["SERVER_PROTOCOL"];
+
+// //Set the site URL.
+// if (SERVER_TYPE == PRODUCTION) {
+//   //The URL of our live website.
+//   define('SITE_URL', 'https://dws-practicas.herokuapp.com/');
+// } elseif (SERVER_TYPE == PRODUCTION_TEST) {
+//   //The URL that we use in our development enviornment.
+//   define('SITE_URL', 'https://dws-practicas-dev.herokuapp.com/');
+// } elseif (SERVER_TYPE == LOCAL) {
+//   define('SITE_URL', 'http://localhost/dws/practicas/');
+// }
+
+define('SITE_URL', $_SERVER["SERVER_NAME"] . '/');
