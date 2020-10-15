12b79793b1620a7f3e9edb9bdfa4e02730eef645
Eduard Stefan <eduardstefan290@gmail.com>
2020-10-15 21:41:46 +0200

    made site_url dynamic. no more manual changes between development and production

--- config.php
+++ config.php
@@ -1,12 +1,5 @@
 <?php
 
-define("PRODUCTION", "PRODUCTION");
-define("PRODUCTION_TEST", "PRODUCTION_TEST");
-define("LOCAL", "LOCAL");
-
-define('SERVER_TYPE', PRODUCTION_TEST);
-
-
 if ($_SERVER["SERVER_NAME"] == "localhost") {
   define('SITE_URL', 'http://' . $_SERVER["SERVER_NAME"] . '/practicas/dws/');
 } else {
