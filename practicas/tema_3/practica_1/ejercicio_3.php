2937c287deef198596749ee6ba5b75a621e673f0
Eduard Stefan <eduardstefan290@gmail.com>
2020-10-21 11:10:09 +0200

    finished ejercicio_3

--- practicas/tema_3/practica_1/ejercicio_3.php
+++ practicas/tema_3/practica_1/ejercicio_3.php
@@ -7,38 +7,62 @@
   <p class="mb-6 is-italic"><?= str_replace("/", "  /  ", $activePageFormated) ?></p>
   <h1 class="title"><?= ucfirst($activePageArr[2]) ?></h1>
   <?php
-  include_once('../../tema_2/practica_2/ejercicio_4/findWord.php');
   include('../../tema_2/practica_2/ejercicio_4/dictionary.php');
   include('../../tema_2/practica_2/ejercicio_4/getIndexOfWord.php');
+  include('../../tema_2/practica_2/ejercicio_4/translateWord.php');
+  include('ejercicio_3/findWord.php');
 
-  if (!isset($_SESSION['word_list_en']) && !isset($_SESSION['word_list_es'])) {
-    $_SESSION['word_list_en'] = $word_list_en;
-    $_SESSION['word_list_es'] = $word_list_es;
+  // session_destroy();
+  $errors = array(
+    'englishWordError' => '',
+    'spanishWordError' => ''
+  );
+
+  $newEnglishWord = '';
+  $newSpanishWord = '';
+
+  // create the dictionaries if they dont exist
+  if (!isset($_SESSION['dict'])) {
+    $_SESSION['dict'] = array_combine($word_list_en, $word_list_es);
   } else {
-    if (isset($_POST['new-english-word']) && isset($_POST['new-spanish-word'])) {
-      $englishWord = $_POST['new-english-word'];
-      $spanishWord = $_POST['new-spanish-word'];
+    // add words to the lists if they don't exist already 
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['new-english-word']) && isset($_POST['new-spanish-word'])) {
+      $newEnglishWord = $_POST['new-english-word'];
+      $newSpanishWord = $_POST['new-spanish-word'];
+      // clear the word in search input
+      $_GET['word'] = null;
 
-      $indexEnglishWord = getIndexOfWord($englishWord, $_SESSION['word_list_en']);
-      $indexSpanishWord = getIndexOfWord($spanishWord, $_SESSION['word_list_es']);
+      $spanishWordExists = findWord($newSpanishWord, $_SESSION['dict']);
+      $englishWordExists = findWord($newEnglishWord, $_SESSION['dict']);
 
-      if ($indexEnglishWord == -1) {
-      } else {
+      if (!is_null($spanishWordExists)) {
+        if ($spanishWordExists[0] == 'spanish') {
+          $errors['spanishWordError'] = 'The word <strong>' . $spanishWordExists[1] . '</strong> was found in the spanish dictionary';
+        } else {
+          $errors['spanishWordError'] = 'The word <strong>' . $spanishWordExists[1] . '</strong> was found in the english dictionary';
+        }
       }
-      $_SESSION['word_list_es'][] = $spanishWord;
+
+      if (!is_null($englishWordExists)) {
+        if ($englishWordExists[0] == 'english') {
+          $errors['englishWordError'] = 'The word <strong>' . $englishWordExists[1] . '</strong> was found in the english dictionary';
+        } else {
+          $errors['englishWordError'] = 'The word <strong>' . $englishWordExists[1] . '</strong> was found in the spanish dictionary';
+        }
+      }
+
+      if ($errors['englishWordError'] == '' && $errors['spanishWordError'] == '') {
+        $_SESSION['dict'][$newEnglishWord] = $newSpanishWord;
+      }
     }
   }
 
-
-  function addWordToDictionary($word, &$dictionary)
-  {
-    $dictionary[] = $word;
-  }
   ?>
-  <div class="">
-    <p>Result: </p>
+  <div>
     <div class="box">
       <h2 class="title is-4">English - Spanish Translator</h2>
+
+      <!-- SEARCH FORM -->
       <form class="columns" action="" method="GET">
         <div class="column is-2 field is-mobile">
           <div class="control has-icons-left has-icons-right">
@@ -54,14 +78,16 @@
           <button class="button is-primary" type="submit">Translate</button>
         </div>
       </form>
+
+      <!-- NOTIFICATIONS -->
       <div>
         <?php
-        if (isset($_GET['word'])) {
-          $foundWord = findWord($_GET['word'], $_SESSION['word_list_en'], $_SESSION['word_list_es']);
-          if (!is_null($foundWord)) {
-            echo '<div class="notification is-primary is-light" style="display:flex; align-items:center">';
+        if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['word'])) {
+          $translatedWord = translateWord($_GET['word'], array_keys($_SESSION['dict']), array_values($_SESSION['dict']));
+          if (!is_null($translatedWord)) {
+            echo '<div class="notification is-success is-light" style="display:flex; align-items:center">';
             echo '  <span class="icon is-size-4 mr-2"><i class="fas fa-check"></i></span>';
-            echo '<span>' . ucfirst($foundWord[0]) . ' word found: <strong>' . $foundWord[1] . '</strong></span>';
+            echo '<span> Word <strong>' . $translatedWord[1] . '</strong> found in ' . $translatedWord[0] . ' dictionary for the word <strong>' . $_GET['word'] . '</strong>.</span>';
             echo '</div>';
           } else {
             echo '<div class="message is-warning is-light" style="max-width: 350px">';
@@ -76,36 +102,67 @@
             echo '  </div>';
             echo '</div>';
           }
+        } elseif ($_SERVER['REQUEST_METHOD'] == 'POST' && !is_null($newEnglishWord)) {
+          echo '<div class="notification is-success is-light" style="display:flex; align-items:center">';
+          echo '  <span class="icon is-size-4 mr-2"><i class="fas fa-check"></i></span>';
+          echo '  <span>The new english word <strong>' . $newEnglishWord . '</strong> with translation in spanish <strong>' . $newSpanishWord . '</strong> was added to the dictionary.</span>';
+          echo '</div>';
         } else {
           echo '<div class="notification is-info is-light" style="display:flex; align-items:center">';
           echo '  <span class="icon is-size-4 mr-2"><i class="fas fa-info-circle"></i></span>';
           echo '  <span>Type a word in english or spanish and see the translation.</span>';
           echo '</div>';
         }
+
         echo '<button id="show-modal-btn" class="button is-primary-light">Add a new word</button>';
-        echo '<pre>' . print_r($_SESSION['word_list_en'], true) . '</pre>';
-        echo '<pre>' . print_r($_SESSION['word_list_es'], true) . '</pre>';
         ?>
       </div>
-      <div id="add-word-modal" class="modal">
+
+      <!-- MODAL ADD NEW WORD FORM -->
+      <div id="add-word-modal" class="modal <?= ($errors['englishWordError'] != '' || $errors['spanishWordError'] != '' ? 'is-active' : '') ?>">
         <div class="modal-background"></div>
         <div class="modal-content">
           <div class="box">
             <form action="" method="POST">
               <h2 class="title is-4">Add a new word</h2>
               <div class="field">
-                <label class="label">Word in spanish</label>
-                <div class="control">
-                  <input required name="new-spanish-word" class="input" type="text" placeholder="Spanish word">
+                <label class="label">Word in english</label>
+                <div class="control has-icons-right">
+                  <?php
+                  $isDanger = $errors['englishWordError'] != '' ? 'is-danger' : '';
+                  $englishValue = $isDanger != '' ? $newEnglishWord : '';
+                  ?>
+                  <input required name="new-english-word" value="<?= $englishValue ?>" class="input <?= $isDanger ?>" type="text" placeholder="English word">
+                  <?php
+                  if ($errors['englishWordError'] != '') {
+                    echo '<span class="icon is-small is-right">';
+                    echo '<i class="fas fa-exclamation-triangle"></i>';
+                    echo '</span>';
+                  }
+                  ?>
                 </div>
+                <p class="help is-danger"><?= $errors['englishWordError'] ?></p>
               </div>
               <div class="field">
-                <label class="label">Word in english</label>
-                <div class="control">
-                  <input required name="new-english-word" class="input" type="text" placeholder="English word">
+                <label class="label">Word in spanish</label>
+                <div class="control has-icons-right">
+                  <?php
+                  $isDanger = $errors['spanishWordError'] != '' ? 'is-danger' : '';
+                  $spanishValue = $isDanger != '' ? $newSpanishWord : '';
+                  ?>
+                  <input required name="new-spanish-word" value="<?= $spanishValue ?>" class="input <?= $isDanger ?>" type="text" placeholder="Spanish word">
+                  <?php
+                  if ($errors['spanishWordError'] != '') {
+                    echo '<span class="icon is-small is-right">';
+                    echo '<i class="fas fa-exclamation-triangle"></i>';
+                    echo '</span>';
+                  }
+                  ?>
                 </div>
+                <p class="help is-danger"><?= $errors['spanishWordError'] ?></p>
               </div>
-              <button class="modla-close button is-primary" type="submit">Add word to dictionary</button>
+
+              <button type="submit" class="button is-primary">Add word to dictionary</button>
             </form>
           </div>
         </div>
--- practicas/tema_3/practica_1/ejercicio_3/popModal.js
+++ practicas/tema_3/practica_1/ejercicio_3/popModal.js
@@ -1,5 +1,8 @@
 let showModalBtn = document.getElementById("show-modal-btn");
 let modal = document.getElementById("add-word-modal");
+let modalBackground = document.querySelector(
+  "#add-word-modal .modal-background"
+);
 let closeModalBtn = document.querySelector("#add-word-modal .modal-close");
 
 showModalBtn.addEventListener("click", openModal);
@@ -7,8 +10,12 @@ closeModalBtn.addEventListener("click", closeModal);
 
 function openModal() {
   modal.classList.add("is-active");
+  modalBackground.addEventListener("click", closeModal, true);
 }
 
-function closeModal() {
-  modal.classList.remove("is-active");
+function closeModal(e) {
+  if (e.target == modalBackground || e.target == closeModalBtn) {
+    modal.classList.remove("is-active");
+    modalBackground.removeEventListener("click", closeModal, true);
+  }
 }
