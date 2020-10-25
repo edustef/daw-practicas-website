361224579c17675ae1377ebf023f6fc03d6a47c4
Eduard Stefan <eduardstefan290@gmail.com>
2020-10-25 13:49:06 +0100

    created template for ejercicio_6

---
+++ practicas/tema_3/practica_1/ejercicio_6.php
@@ -0,0 +1,16 @@
+<?php include_once(__DIR__ . "/../../../templates/header.php") ?>
+<div class="block">
+  <?php
+  $activePageFormated = explode(".", str_replace("_", " ", $activePage))[0];
+  $activePageArr = explode("/", $activePageFormated);
+  ?>
+  <p class="mb-6 is-italic"><?= str_replace("/", "  /  ", $activePageFormated) ?></p>
+  <h1 class="title"><?= ucfirst($activePageArr[2]) ?></h1>
+  <?php
+
+  ?>
+  <div class="box">
+
+  </div>
+</div>
+<?php include_once(__DIR__ . "/../../../templates/footer.php") ?>
