14537d4cc127515573025e6e821bfa34a78d7ed1
Eduard Stefan <eduardstefan290@gmail.com>
2020-11-04 00:44:15 +0100

    added juegaciam

---
+++ practicas/tema_3/practica_2/juegaciam.php
@@ -0,0 +1,18 @@
+<?php include_once(__DIR__ . "/../../../templates/header.php") ?>
+<div class="block">
+  <?php
+  $activePageFormated = explode(".", str_replace("_", " ", $activePage))[0];
+  $activePageArr = explode("/", $activePageFormated);
+  ?>
+  <p class="mb-2 is-italic"><?= str_replace("/", "  /  ", $activePageFormated) ?></p>
+  <h1 class="title"><?= ucfirst($activePageArr[2]) ?></h1>
+  <?php
+
+  ?>
+  <div class="box">
+    <a href="https://juegociam.herokuapp.com/">Go to website hosted on Heroku</a>
+    <hr>
+    <a href="https://github.com/bilalbejja-collab/juegaciam2020">See the source code on GitHub</a>
+  </div>
+</div>
+<?php include_once(__DIR__ . "/../../../templates/footer.php") ?>
