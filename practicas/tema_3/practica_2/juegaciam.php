a3adbe019739a0e5cc0855ceca0e31453c07bb22
Eduard Stefan <eduardstefan290@gmail.com>
2020-11-04 00:47:51 +0100

    updated juegaciam

--- practicas/tema_3/practica_2/juegaciam.php
+++ practicas/tema_3/practica_2/juegaciam.php
@@ -10,9 +10,9 @@
 
   ?>
   <div class="box">
-    <a href="https://juegociam.herokuapp.com/">Go to website hosted on Heroku</a>
+    <a href="https://juegociam.herokuapp.com/" target="_blank">Go to website hosted on Heroku</a>
     <hr>
-    <a href="https://github.com/bilalbejja-collab/juegaciam2020">See the source code on GitHub</a>
+    <a href="https://github.com/bilalbejja-collab/juegaciam2020" target="_blank">See the source code on GitHub</a>
   </div>
 </div>
 <?php include_once(__DIR__ . "/../../../templates/footer.php") ?>
